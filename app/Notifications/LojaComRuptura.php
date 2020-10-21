0824a98f1415216209ab6d88132d9955f919513d
Juan.Carlos <juan.carlos@abrascort.com.br>
2020-10-21 15:22:15 -0300

    setando notificacao como queue

--- app/Notifications/LojaComRuptura.php
+++ app/Notifications/LojaComRuptura.php
@@ -8,7 +8,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
 
-class LojaComRuptura extends Notification
+class LojaComRuptura extends Notification implements ShouldQueue
 {
     use Queueable;
 
